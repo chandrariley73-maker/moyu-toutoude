7d69d68a9a27297b81abd5c52f83ba1e710a0b61
chandrariley73-maker <chandrariley73@gmail.com>
2026-04-24 10:51:21 +0800

    Add files via upload

---
+++ work_my/core/src/main/java/com/my/game/PixelGame.java
@@ -0,0 +1,115 @@
+package com.my.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+
+public class PixelGame extends ApplicationAdapter {
+    SpriteBatch batch;
+    OrthographicCamera camera;
+
+    // 玩家
+    Rectangle player;
+    float speed = 300f;
+
+    // 精灵表动画相关 → 全部提升为成员变量
+    Texture walkSheet;
+    Animation<TextureRegion> walkAnimation;
+    TextureRegion[] walkFrames; // 这里加上！
+    float stateTime;
+
+    @Override
+    public void create() {
+        // 创建SpriteBatch实例，用于绘制精灵
+        batch = new SpriteBatch();
+        // 创建正交相机，设置视口大小为800x600
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 600);
+
+        // 加载精灵表
+        walkSheet = new Texture("player_walk.png");
+
+        int frameWidth = 32;
+        int frameHeight = 32;
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet, frameWidth, frameHeight);
+        walkFrames = tmp[0];
+
+        // 创建动画
+        walkAnimation = new Animation<>(0.15f, walkFrames);
+        stateTime = 0f;
+
+        // 玩家
+        player = new Rectangle();
+        player.x = 400;
+        player.y = 300;
+        player.width = 64;
+        player.height = 64;
+    }
+
+    @Override
+/**
+ * 渲染方法，用于绘制游戏画面
+ * 该方法负责清除屏幕、处理玩家移动、更新动画状态以及绘制玩家角色
+ */
+    public void render() {
+    // 设置屏幕背景颜色为深灰色(R:0.15, G:0.15, B:0.15, A:1)
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.15f, 1);
+    // 清除屏幕，使用GL_COLOR_BUFFER_BIT位来清除颜色缓冲区
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    // 获取自上一帧以来经过的时间(秒)
+        float delta = Gdx.graphics.getDeltaTime();
+    // 更新状态时间，用于动画帧计算
+        stateTime += delta;
+
+        // 移动相关变量初始化
+        boolean moving = false;
+    // 检测W键是否被按下，如果按下则向上移动玩家
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            player.y += speed * delta;
+            moving = true;
+        }
+    // 检测S键是否被按下，如果按下则向下移动玩家
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            player.y -= speed * delta;
+            moving = true;
+        }
+    // 检测A键是否被按下，如果按下则向左移动玩家
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            player.x -= speed * delta;
+            moving = true;
+        }
+    // 检测D键是否被按下，如果按下则向右移动玩家
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            player.x += speed * delta;
+            moving = true;
+        }
+
+        // 获取当前帧
+        TextureRegion currentFrame;
+        if (moving) {
+            currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        } else {
+            currentFrame = walkFrames[0];
+        }
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(currentFrame, player.x, player.y, 32, 32);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        walkSheet.dispose();
+    }
+}
